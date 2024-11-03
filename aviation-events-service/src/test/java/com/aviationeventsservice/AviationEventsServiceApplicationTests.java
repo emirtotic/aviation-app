54915235e5b64f6410ebd30a80d53a3b21562720
emirtotic <emirtotic@gmail.com>
2024-11-03 21:02:06 +0100

    Refactor

---
+++ aviation-events-service/src/test/java/com/aviationeventsservice/AviationEventsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviationeventsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviationEventsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
